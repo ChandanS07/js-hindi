0ce88ab422b0f54e49b9d9e1c6fe8b80dffaa4e3
ChandanS07 <143780373+ChandanS07@users.noreply.github.com>
2025-03-30 09:44:36 +0000

    .

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "chandan@gmail.com"
+var accountPassword = "12345"
+accountCty = "Mumbai"
+
+let autom;
+// accountId = 2
+// console.log(accountId);
+accountEmail = "cls@cls.com"
+accountPassword = "2580"
+accountCty = "Patna"
+
+console.table([accountId, accountEmail, accountPassword, accountCty, autom])
